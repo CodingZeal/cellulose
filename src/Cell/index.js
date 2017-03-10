3bae6fd907da3773d977f94a55d93f70955573fa
Byron Matto <bmatto@gmail.com>
2017-03-10 14:52:37 -0500

    Make Cell stateless functional component

--- src/Cell/index.js
+++ src/Cell/index.js
@@ -17,37 +17,35 @@ const StyledCell = styled.div`
   box-sizing: border-box;
   flex: 0 0 auto;
 `
-export default class Cell extends PureComponent {
-  flexProperties = [
-    'alignSelf',
-    'flexBasis',
-    'flexGrow',
-    'flexShrink',
-    'flex',
-    'order'
-  ]
+const flexProperties = [
+  'alignSelf',
+  'flexBasis',
+  'flexGrow',
+  'flexShrink',
+  'flex',
+  'order'
+]
 
-  render() {
-    const { breakPoint, children, columns } = this.props
-    const spanOptions = over(
-      lensProp('0'),
-      defaultTo(columns),
-      this.props.spanOptions
-    )
-    const span = prop(breakPoint, spanOptions)
-    const cols = when(is(Object), prop('cols'))(span)
-    const className = ifElse(is(Object), prop('className'), F)(span)
-    const flexStyles = pick(this.flexProperties)(this.props)
-    const width = `${cols / columns * 100}%`
+export default function Cell(props) {
+  const { breakPoint, children, columns, ...rest} = props
+  const spanOptions = over(
+    lensProp('0'),
+    defaultTo(columns),
+    props.spanOptions
+  )
+  const span = prop(breakPoint, spanOptions)
+  const cols = when(is(Object), prop('cols'))(span)
+  const className = ifElse(is(Object), prop('className'), F)(span)
+  const flexStyles = pick(flexProperties)(rest)
+  const width = `${cols / columns * 100}%`
 
-    const cellClassName = classnames(
-      { [className]: className }
-    )
+  const cellClassName = classnames(
+    { [className]: className }
+  )
 
-    return (
-      <StyledCell style={{ ...flexStyles, width }} className={cellClassName}>
-        {children}
-      </StyledCell>
-    )
-  }
+  return (
+    <StyledCell style={{ ...flexStyles, width }} className={cellClassName}>
+      {children}
+    </StyledCell>
+  )
 }
