22796088b5a59552606ef7097fc551b6cfa3e735
Frank West <frank.west@codingzeal.com>
2017-05-19 14:42:45 -0700

    Install cellulose and start implementing doc site

--- site/client/modules/app/components/App/Features/index.js
+++ site/client/modules/app/components/App/Features/index.js
@@ -1,27 +1,33 @@
 import React from 'react'
+import { Cell, Cellulose } from 'cellulose'
 
 export default function Header(props) {
+  const breakPoints = {
+    0: 'greater-than-0',
+    480: 'greater-than-480'
+  }
+
   return (
     <section>
-      <div>
-        <div>
+      <Cellulose columns={3} breakPoints={breakPoints}>
+        <Cell spanOptions={{ 0: 3, 480: 1 }}>
           <div>MQ Icon</div>
           <h3>Better Than Media Queries</h3>
           <p>Components are responsive to their own size, rather than window size</p>
-        </div>
+        </Cell>
 
-        <div>
+        <Cell spanOptions={{ 0: 3, 480: 1 }}>
           <div>Granular Icon</div>
           <h3>Granular</h3>
           <p>Your custom components can each manage their own grid</p>
-        </div>
+        </Cell>
 
-        <div>
+        <Cell spanOptions={{ 0: 3, 480: 1 }}>
           <div>BP Icon</div>
           <h3>Easy Breakpoints</h3>
           <p>Define breakpoints with a simple Javascript object, give each UI component its own custom grid</p>
-        </div>
-      </div>
+        </Cell>
+      </Cellulose>
     </section>
   )
 }
--- site/client/modules/app/components/App/index.js
+++ site/client/modules/app/components/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Cell, Cellulose } from 'cellulose'
 
 import styles from './styles.scss'
 import Features from './Features'
@@ -6,11 +7,76 @@ import Header from './Header'
 import Hero from './Hero'
 
 export default function App({ store }) {
+  const breakPoints = {
+    0: 'greater-than-0',
+    480: 'greater-than-480'
+  }
   return (
     <div className={styles.root}>
       <Header />
       <Hero />
       <Features />
+      <Cellulose columns={2} breakPoints={breakPoints}>
+        <Cell spanOptions={{ 0: 2, 480: 1 }}>
+          <h3>Getting Started</h3>
+          <p>
+            <span className={styles.tag}>
+              &lt;</span>
+            <strong>Cellulose</strong>
+            <span className={styles.tag}>
+              &gt;
+            </span>
+            &nbsp;will render into the DOM as a&nbsp;
+            <span className={styles.tag}>
+              &lt;
+            </span>
+            <strong>div</strong>
+            <span className={styles.tag}>
+              &gt;
+            </span>
+            &nbsp;whose class is defined by the breakPoints object and the
+            width the component exceeds.
+          </p>
+        </Cell>
+        <Cell spanOptions={{ 0: 2, 480: 1 }}>
+          <pre>CodeBlock</pre>
+        </Cell>
+      </Cellulose>
+      <Cellulose columns={2} breakPoints={breakPoints}>
+        <Cell spanOptions={{ 0: 2, 480: 1 }}>
+          <h3>Installation</h3>
+          <h4>Npm</h4>
+        </Cell>
+        <Cell spanOptions={{ 0: 2, 480: 1 }}>
+          <h4>Yarn</h4>
+        </Cell>
+      </Cellulose>
+      <Cellulose columns={2} breakPoints={breakPoints}>
+        <Cell spanOptions={{ 0: 2, 480: 1 }}>
+          <h3>Usage</h3>
+          <p>
+            Cellulose can also be used to create a responsive grid. Use the
+            columns property of
+            <span className={styles.tag}>
+              &lt;</span>
+            <strong>Cellulose</strong>
+            <span className={styles.tag}>
+              &gt;
+            </span>
+            to define the number of columns to use, then add
+            <span className={styles.tag}>
+              &lt;</span>
+            <strong>Cell</strong>
+            <span className={styles.tag}>
+              &gt;
+            </span>
+            components with spanOptions props that define responsive behavior
+          </p>
+        </Cell>
+        <Cell spanOptions={{ 0: 2, 480: 1 }}>
+          <pre>CodeBlock</pre>
+        </Cell>
+      </Cellulose>
     </div>
   )
 }
