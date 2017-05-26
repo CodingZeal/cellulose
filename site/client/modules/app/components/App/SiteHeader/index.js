9fedceb6a7a96bbcd6e1df988de6238e0719531d
Chris White <chris.white@codingzeal.com>
2017-05-26 14:52:05 -0600

    Makes site header responsive using Cellulose

--- site/client/modules/app/components/App/SiteHeader/index.js
+++ site/client/modules/app/components/App/SiteHeader/index.js
@@ -1,33 +1,40 @@
+import { Cellulose, Cell } from 'cellulose'
 import React from 'react'
 
 import celluloseLogo from '../Header/cellulose-logo.png'
 import reactgenLogo from './react-gen-logo.png'
 import styles from './styles.scss'
 
+const breakPoints = {
+  480: `${styles.containerShown}`
+}
+
 export default function SiteHeader(props) {
   return (
-    <div className={styles.container}>
-      <div>
-        <ul className={styles.linkList}>
-          <li>
-            <a href="http://open.codingzeal.com/generator-react-zeal/">
-              <img src={reactgenLogo} />
-              ReactGen
-            </a>
-          </li>
-          <li className={styles.active}>
-            <a href="http://getcellulose.com/">
-              <img src={celluloseLogo} />
-              Cellulose
-            </a>
-          </li>
-        </ul>
+    <Cellulose breakPoints={breakPoints}>
+      <div className={styles.container}>
+        <div>
+          <ul className={styles.linkList}>
+            <li>
+              <a href="http://open.codingzeal.com/generator-react-zeal/">
+                <img src={reactgenLogo} />
+                ReactGen
+              </a>
+            </li>
+            <li className={styles.active}>
+              <a href="http://getcellulose.com/">
+                <img src={celluloseLogo} />
+                Cellulose
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.sideCredit}>
+          Maintained by the development team at
+          {' '}
+          <a href="https://codingzeal.com">Zeal</a>
+        </div>
       </div>
-      <div className={styles.sideCredit}>
-        Maintained by the development team at
-        {' '}
-        <a href="https://codingzeal.com">Zeal</a>
-      </div>
-    </div>
+    </Cellulose>
   )
 }
