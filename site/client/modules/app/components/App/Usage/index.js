60c5bf24fb5895623d0785a704fd4c5b14881757
Frank West <frank.west@codingzeal.com>
2017-06-23 13:10:11 -0700

    Move usage into separate component

---
+++ site/client/modules/app/components/App/Usage/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Cell, Cellulose } from "cellulose";
+
+import styles from "./styles.scss";
+
+export default function Usage() {
+  const breakPoints = {
+    0: "greater-than-0",
+    480: "greater-than-480"
+  };
+
+  return (
+    <section className={styles.usageContainer}>
+      <div className={styles.usage}>
+        <Cellulose columns={2} breakPoints={breakPoints}>
+          <Cell spanOptions={{ 0: 2, 480: 1 }}>
+            <h3>Usage</h3>
+            <p>
+              Cellulose can also be used to create a responsive grid. Use the
+              columns property of
+              <span className={styles.leftPaddedTag}>
+                &lt;
+              </span>
+              <strong>Cellulose</strong>
+              <span className={styles.rightPaddedTag}>
+                &gt;
+              </span>
+              to define the number of columns to use, then add
+              <span className={styles.leftPaddedTag}>
+                &lt;
+              </span>
+              <strong>Cell</strong>
+              <span className={styles.rightPaddedTag}>
+                &gt;
+              </span>
+              components with spanOptions props that define responsive behavior
+            </p>
+          </Cell>
+          <Cell spanOptions={{ 0: 2, 480: 1 }}>
+            <pre className="prettyprint">
+              {`import { Cellulose, Cell } from 'cellulose'
+import React from 'react'
+export function MyComponent() {
+  const breakPoints = {
+    768:  'greater-than-768',
+    1024: 'greater-than-1024'
+  }
+  return (
+    <Cellulose columns={12} breakPoints={breakPoints}>
+      <Cell spanOptions={{ 768: 6, 1024: 8 }}>
+        <div>One</div>
+      </Cell>
+      <Cell spanOptions={{ 768: 6, 1024: 4 }}>
+        <div>Two</div>
+      </Cell>
+    </Cellulose>
+  )
+}`}
+            </pre>
+          </Cell>
+        </Cellulose>
+      </div>
+    </section>
+  );
+}
--- site/client/modules/app/components/App/index.js
+++ site/client/modules/app/components/App/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Cell, Cellulose } from "cellulose";
 
 import styles from "./styles.scss";
 
@@ -9,13 +8,9 @@ import Header from "./Header";
 import Hero from "./Hero";
 import Installation from "./Installation";
 import SiteHeader from "./SiteHeader";
+import Usage from "./Usage";
 
 export default function App() {
-  const breakPoints = {
-    0: "greater-than-0",
-    480: "greater-than-480"
-  };
-
   return (
     <div className={styles.root}>
       <SiteHeader />
@@ -24,34 +19,7 @@ export default function App() {
       <Features />
       <GettingStarted />
       <Installation />
-      <Cellulose columns={2} breakPoints={breakPoints}>
-        <Cell spanOptions={{ 0: 2, 480: 1 }}>
-          <h3>Usage</h3>
-          <p>
-            Cellulose can also be used to create a responsive grid. Use the
-            columns property of
-            <span className={styles.tag}>
-              &lt;
-            </span>
-            <strong>Cellulose</strong>
-            <span className={styles.tag}>
-              &gt;
-            </span>
-            to define the number of columns to use, then add
-            <span className={styles.tag}>
-              &lt;
-            </span>
-            <strong>Cell</strong>
-            <span className={styles.tag}>
-              &gt;
-            </span>
-            components with spanOptions props that define responsive behavior
-          </p>
-        </Cell>
-        <Cell spanOptions={{ 0: 2, 480: 1 }}>
-          <pre>CodeBlock</pre>
-        </Cell>
-      </Cellulose>
+      <Usage />
     </div>
   );
 }
